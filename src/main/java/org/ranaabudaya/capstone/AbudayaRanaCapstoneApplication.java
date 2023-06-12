67cec7d0a9b17f6643446781bf77f036f1a18a64
Rana <rananaser94@gmail.com>
2023-06-12 12:32:09 -0700

    first commit

---
+++ src/main/java/org/ranaabudaya/capstone/AbudayaRanaCapstoneApplication.java
@@ -0,0 +1,13 @@
+package org.ranaabudaya.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbudayaRanaCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbudayaRanaCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ranaabudaya/capstone/AbudayaRanaCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ranaabudaya.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbudayaRanaCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
